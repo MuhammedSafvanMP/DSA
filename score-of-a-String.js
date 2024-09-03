39da51eb77c975b8e5976c127ce98eae4ebcd65d
MUHAMMED SAFVAN MP <muhammedsafvanmp7@gmail.com>
2024-09-03 21:36:41 +0530

    3110. Score of a String

---
+++ score-of-a-String.js
@@ -0,0 +1,15 @@
+var scoreOfString = function(s) {
+      
+    let result = 0;
+      for (let i = 0; i < s.length - 1; i++ ){
+         result +=  Math.abs( s.charCodeAt(i) - s.charCodeAt(i + 1))
+      }
+
+      return result
+};
+
+ let s = "hello"
+
+ console.log(scoreOfString(s));
+ 
+ 3110. Score of a String
